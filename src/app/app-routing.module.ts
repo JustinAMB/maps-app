3bed93c9c9a0f90f7fdf17236f662441b3566bc7
JustinAMB <Justinmb@outlook.es>
2022-02-14 16:34:57 -0600

    se define la ruta principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { MapsRoutingModule } from './maps/maps-routing.module';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [MapsRoutingModule,RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
